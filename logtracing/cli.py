68b167c49993e7d96563a3bddacc982a1cbb7b41
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-07-14 12:35:35 -0600

    Add a new option to overwrite the existing config file

--- logtracing/cli.py
+++ logtracing/cli.py
@@ -15,32 +15,33 @@ def _version_callback(value: bool):
 @app.command()
 def init(
     db_user: str = typer.Option(
-        None,
+        ...,
         "--db-user",
-        prompt="Database user",
     ),
     db_password: str = typer.Option(
-        None,
+        ...,
         "--db-pass",
-        prompt="Database password",
     ),
     db_host: str = typer.Option(
-        None,
+        ...,
         "--db-host",
-        prompt="Database host",
     ),
     db_port: str = typer.Option(
-        None,
+        ...,
         "--db-port",
-        prompt="Database port",
     ),
     db_database: str = typer.Option(
-        None,
+        ...,
         "--db-name",
-        prompt="Database name",
+    ),
+    force: Optional[bool] = typer.Option(
+        False,
+        "--force",
+        "-f",
+        help="Overwrite the current configuration",
     )
     ) -> None:
-    if not config.exists():
+    if not config.exists() or force:
         db_config = {
             'user': db_user,
             'pass': db_password,
@@ -52,14 +53,13 @@ def init(
         status = config.init(db_config=db_config)
 
         if not status == SUCCESS:
-            print(f"\n[bold red]Something went wrong with '{ERRORS[status]}'[/bold red]\n")
+            print(f"\n[red]Something went wrong with '{ERRORS[status]}'[/red]\n")
             raise typer.Exit(1)
 
         print('\n[green]Initial configuration was done![green]\n')
         raise typer.Exit()
     else:
-        print("\n[bold red]The config file already exists[/bold red]\n")
-    
+        print("\n[red]The config file already exists. Use the option --force or -f to overwrite it.[/red]\n")
 
 @app.command()
 def main(
